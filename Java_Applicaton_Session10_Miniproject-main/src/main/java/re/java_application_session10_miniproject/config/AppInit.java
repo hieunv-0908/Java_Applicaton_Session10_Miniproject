ce38b885b3014d55fc043e87a8bbc54f7a16a5d4
duonvipbn <duonvipbn@gmail.com>
2026-04-16 22:45:48 +0700

    Add files via upload

---
+++ Java_Applicaton_Session10_Miniproject-main/src/main/java/re/java_application_session10_miniproject/config/AppInit.java
@@ -0,0 +1,21 @@
+package re.java_application_session10_miniproject.config;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
