ed35d558898f46fa64a6b50d2ad3f56fc03c9d5e
huyen <tkhuyen1303@gmail.com>
2026-04-16 23:03:29 +0700

    huyen

--- src/main/java/re/java_application_session10_miniproject/config/WebConfig.java
+++ src/main/java/re/java_application_session10_miniproject/config/WebConfig.java
@@ -1,64 +1,61 @@
 package re.java_application_session10_miniproject.config;
 
-import org.jspecify.annotations.Nullable;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.validation.Validator;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("re")
+@ComponentScan(basePackages = "re.java_application_session10_miniproject")
 public class WebConfig implements WebMvcConfigurer {
 
-    // Thymeleaf
+    private final ApplicationContext applicationContext;
+
+    public WebConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Bean
     public SpringResourceTemplateResolver templateResolver() {
-        var resolver = new SpringResourceTemplateResolver();
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setApplicationContext(applicationContext);
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML");
+        resolver.setTemplateMode(TemplateMode.HTML);
         resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
         return resolver;
     }
 
     @Bean
     public SpringTemplateEngine templateEngine() {
-        var engine = new SpringTemplateEngine();
+        SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.setTemplateResolver(templateResolver());
+        engine.setEnableSpringELCompiler(true);
         return engine;
     }
 
     @Bean
-    public ThymeleafViewResolver viewResolver() {
-        var resolver = new ThymeleafViewResolver();
+    public ViewResolver viewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8");
+        resolver.setOrder(1);
         return resolver;
     }
 
-    // Static resource (css, js)
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("/resources/static/");
-    }
-
-    // Validation
-    @Bean
-    public LocalValidatorFactoryBean validator() {
-        return new LocalValidatorFactoryBean();
-    }
-
-    @Override
-    public @Nullable Validator getValidator() {
-        return validator();
     }
 }
